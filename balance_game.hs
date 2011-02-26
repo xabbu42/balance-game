b698477154339dfd7f3a2d871395b181a2156cbf
Nathan Gass <gass@search.ch>
2011-02-26 12:08:13 +0100

    Actually construct and print solution trees.

--- balance_game.hs
+++ balance_game.hs
@@ -6,6 +6,7 @@ import qualified Data.Map as M
 import Maybe
 import Monad
 import Text.ParserCombinators.Parsec
+import Text.PrettyPrint.HughesPJ
 
 import qualified System.Console.CmdArgs as CMD
 import System.Console.CmdArgs ((&=))
@@ -23,6 +24,7 @@ import Criterion.Config hiding (Normal)
 
 data Args = Calc { positions :: [String]
                  , perfect   :: Bool
+                 , solution  :: Bool
                  }
           | Test
           | Benchmark
@@ -33,6 +35,8 @@ args = CMD.modes
               , perfect   = False &= CMD.name "p"
                                   &= CMD.help ("Only consider solutions where it is known"
                                                ++ " if the ball is too heavy or too light.")
+              , solution  = False &= CMD.name "s"
+                                  &= CMD.help ("Show the best solution")
               } &= CMD.auto
                 &= CMD.details [ "In the balance game, you need to find the *one*"
                                  ++ " ball with the wrong weight among a given number"
@@ -48,17 +52,21 @@ main :: IO ()
 main = do
   args <- CMD.cmdArgs args
   case args of
-    Calc {positions = [], perfect = p} -> handle_pos p $ start_position 13
-    Calc {positions = ps, perfect = p} -> sequence_ $ map (handle_pos p . read) ps
+    Calc {positions = [], perfect = p, solution = s} -> handle_pos p s $ start_position 13
+    Calc {positions = ps, perfect = p, solution = s} -> sequence_ $ map (handle_pos p s . read) ps
     Test                               -> withArgs [] $ $(defaultMainGenerator)
     Benchmark                          -> withArgs [] $ defaultMain benchmarks
   where
-    handle_pos p pos  = putStrLn $ show pos ++ ": "
-                        ++ (show $ (if p then depth_perfect else depth_unknown) $ pos)
-    benchmarks        = concat $ map benchmark_pos ["U3", "U5", "U13"]
-    benchmark_pos s   = [ bench ("unknown " ++ s) $ whnf depth_unknown (read s)
-                        , bench ("perfect " ++ s) $ whnf depth_perfect (read s)
-                        ]
+    cond True  = is_perfect_solution
+    cond False = is_solution
+    result True  p pos = "\n" ++ (show $ solution_with (cond p) pos)
+    result False p pos = show $ depth_with (cond p) pos
+    handle_pos p s pos = putStrLn $ show pos ++ ": " ++ result s p pos ++ "\n"
+    benchmarks         = concat $ map benchmark_pos ["U3", "U5", "U13"]
+    benchmark_pos s    = [ bench ("unknown depth " ++ s) $ whnf depth_unknown (read s)
+                         , bench ("perfect depth " ++ s) $ whnf depth_perfect (read s)
+                         , bench ("unknown solution " ++ s) $ whnf solution_unknown (read s)
+                         ]
 
 data Position = Position { unknown :: Int -- balls with completely unknown weight
                          , heavy   :: Int -- balls which could be heavy but not light
@@ -71,7 +79,8 @@ data Label = Unknown | Heavy | Light | Normal
 instance Show Position where
   show p = let f c l = case get_label l p of
                             0 -> ""
-                            n -> c:show n
+                            n | n < 4     -> take n $ repeat c
+                              | otherwise -> '(' : c : show n ++ ")"
            in concat $ zipWith f  "UHLN" [Unknown, Heavy, Light, Normal]
 
 instance Read Position where
@@ -92,7 +101,7 @@ instance Read Position where
 instance Arbitrary Position where
   arbitrary = sized $ \n -> do
     let n' = (n + 3) `min` 10
-    labels <- sequence $ replicate n' $ oneof $ map return all_labels 
+    labels <- sequence $ replicate n' $ oneof $ map return all_labels
     return $ foldl (\p l -> set_label l (length $ filter (== l) labels) p) empty_position all_labels
 
 data QCPosMove = QCPosMove Position Move
@@ -184,7 +193,7 @@ apply_move :: Position -> Move -> [Position]
 apply_move pos move = nub [apply_result res pos move | res <- [Equal, Left, Right]]
 
 apply_result :: Result -> Position -> Move -> Position
-apply_result Equal p (Move l r) = let p' = p `sub` l `sub` r 
+apply_result Equal p (Move l r) = let p' = p `sub` l `sub` r
                                   in p' {normal = normal p' + total l + total r}
 apply_result Left  p (Move le ri) = let he = unknown le + heavy le
                                         li = unknown ri + light ri
@@ -213,15 +222,21 @@ good_moves pos = filter (not . bad_move pos) $ possible_moves pos
 prop_good_moves :: Position -> Bool
 prop_good_moves p = (normal p >= total p - 1) == (null $ good_moves p)
 
+is_solution :: Position -> Bool
+is_solution p = normal  p >= total p - 1
+
 depth_unknown :: Position -> Int
-depth_unknown = depth_with $ \p -> (normal p >= total p - 1)
+depth_unknown = depth_with is_solution
 
 case_depth_unknown_4  = 2 @=? depth_unknown (start_position 4)
 case_depth_unknown_13 = 3 @=? depth_unknown (start_position 13)
 case_depth_unknown_14 = 4 @=? depth_unknown (start_position 14)
 
+is_perfect_solution :: Position -> Bool
+is_perfect_solution p = normal p >= total p - 1 && unknown p == 0
+
 depth_perfect :: Position -> Int
-depth_perfect = depth_with $ \p -> (normal p >= total p - 1 && unknown p == 0)
+depth_perfect = depth_with is_perfect_solution
 
 case_depth_perfect_4  = 3 @=? depth_perfect (start_position 4)
 case_depth_perfect_12 = 3 @=? depth_perfect (start_position 12)
@@ -253,3 +268,50 @@ depth_ord Nothing  Nothing  = EQ
 depth_ord _        Nothing  = LT
 depth_ord Nothing  _        = GT
 depth_ord (Just a) (Just b) = a `compare` b
+
+data Tree = Branch { move :: Move
+                   , position :: Position
+                   , left :: Tree
+                   , equal :: Tree
+                   , right :: Tree
+                   }
+          | Node Position
+
+solution_with :: (Position -> Bool) -> Position -> Tree
+solution_with cond pos
+  | (normal pos >= total pos - 1 && unknown pos == 0) = Node pos
+  | otherwise = Branch { move     = best_move
+                       , position = pos
+                       , left     = solution_with cond $ apply_result Left  pos best_move
+                       , equal    = solution_with cond $ apply_result Equal pos best_move
+                       , right    = solution_with cond $ apply_result Right pos best_move
+                       }
+  where
+    best_move     = fst $ minimumBy moves_ord $ map add_depth $ good_moves pos
+    add_depth m   = (m, maximum $ map (depth_with cond) $ apply_move pos m)
+    moves_ord a b = snd a `compare` snd b
+
+solution_unknown :: Position -> Tree
+solution_unknown = solution_with is_solution
+
+solution_perfect :: Position -> Tree
+solution_perfect = solution_with is_perfect_solution
+
+pretty_print :: Tree -> String
+pretty_print = show . tree_doc
+  where
+    tree_doc Branch { move     = Move ml mr
+                    , position = p
+                    , left     = l
+                    , equal    = e
+                    , right    = r
+                    }
+      = text (show p ++ ": " ++ show ml ++ " _|_ " ++ show mr)
+        $$ text "Left: " <+> tree_doc l
+        $$ text "Equal:" <+> tree_doc e
+        $$ text "Right:" <+> tree_doc r
+    tree_doc (Node pos)
+      = text (show pos)
+
+instance Show Tree where
+  show t = pretty_print t
